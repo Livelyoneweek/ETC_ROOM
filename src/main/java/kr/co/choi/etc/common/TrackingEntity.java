043a3d3dfbd83781f9676f9ac3c7b32921bf352f
Livelyoneweek <wlvvwhs@naver.com>
2025-03-13 14:23:33 +0900

    ⚡️ H2 및 jpa 주석 해제
    예시코드 추가, 트랜잭셔널 테스트용

--- src/main/java/kr/co/choi/etc/common/TrackingEntity.java
+++ src/main/java/kr/co/choi/etc/common/TrackingEntity.java
@@ -1,39 +1,39 @@
-//package kr.co.choi.etc.common;
-//
-//import com.fasterxml.jackson.annotation.JsonFormat;
-//import com.fasterxml.jackson.annotation.JsonProperty;
-//import jakarta.persistence.EntityListeners;
-//import jakarta.persistence.MappedSuperclass;
-//import lombok.Getter;
-//import org.springframework.data.annotation.CreatedBy;
-//import org.springframework.data.annotation.CreatedDate;
-//import org.springframework.data.annotation.LastModifiedBy;
-//import org.springframework.data.annotation.LastModifiedDate;
-//import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-//
-//import java.time.LocalDateTime;
-//import java.util.UUID;
-//
-//@EntityListeners(value = {AuditingEntityListener.class})
-//@MappedSuperclass
-//@Getter
-//public abstract class TrackingEntity {
-//    @JsonProperty("created-by")
-//    @CreatedBy
-//    private UUID createdBy;
-//
-//    @JsonProperty("created-date")
-//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-//    @CreatedDate
-//    private LocalDateTime createdDate;
-//
-//    @JsonProperty("last-modified-by")
-//    @LastModifiedBy
-//    private UUID lastModifiedBy;
-//
-//    @JsonProperty("last-modified-date")
-//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-//    @LastModifiedDate
-//    private LocalDateTime lastModifiedDate;
-//
-//}
+package kr.co.choi.etc.common;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@EntityListeners(value = {AuditingEntityListener.class})
+@MappedSuperclass
+@Getter
+public abstract class TrackingEntity {
+    @JsonProperty("created-by")
+    @CreatedBy
+    private UUID createdBy;
+
+    @JsonProperty("created-date")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @JsonProperty("last-modified-by")
+    @LastModifiedBy
+    private UUID lastModifiedBy;
+
+    @JsonProperty("last-modified-date")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+}
