f8c464bfbe6a47ae218d799d55bd8aa1f001ebda
Livelyoneweek <wlvvwhs@naver.com>
2025-07-01 17:40:17 +0900

    ⚡️ File db 추가
    db 추가

--- src/main/java/kr/co/choi/etc/config/RequestIdInterceptor.java
+++ src/main/java/kr/co/choi/etc/config/RequestIdInterceptor.java
@@ -2,6 +2,7 @@ package kr.co.choi.etc.config;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -13,19 +14,51 @@ import java.util.UUID;
  * 로그 콘솔 패턴 -> [%highlight(%-5level)] [%d{yyyy-MM-dd HH:mm:ss}] [%thread] [%X{txId}] [%logger{0}:%line] %msg%n , 해당 부분 [%X{txId}] 에서 값 표출
  */
 @Component
+@Slf4j
 public class RequestIdInterceptor implements HandlerInterceptor {
 
     private static final String TX_ID = "txId";
+    private static final String START_TIME = "startTime";
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        // txId 생성 & MDC 에 저장
         String txId = UUID.randomUUID().toString();
         MDC.put(TX_ID, txId);
+
+        // 시작 시간 기록
+        request.setAttribute(START_TIME, System.currentTimeMillis());
+
         return true;
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
-        MDC.remove(TX_ID);
+        try {
+            long start = (Long) request.getAttribute(START_TIME);
+            long latency = System.currentTimeMillis() - start;
+
+            int status = response.getStatus();
+            String method = request.getMethod();
+            String uri = request.getRequestURI();
+//            String domain = DomainEnum.getDomainByUri(uri);
+
+            // SecurityContext 에서 userId 추출
+//            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//            String userId = (auth != null && auth.isAuthenticated())
+//                    ? auth.getName()
+//                    : "anonymous";
+
+            // 로그 출력 (structured logging 사용 시 JSON encoder 자동 포맷팅)
+            log.info("### ☘️ [API_RES_INFO] : mobile={}, method={},  uri={}, status={}, latencyMs={}",
+                    MDC.get(TX_ID),
+                    method,
+                    uri,
+                    status,
+                    latency);
+        } finally {
+            // MDC 정리
+            MDC.remove(TX_ID);
+        }
     }
 }
--- src/main/java/kr/co/choi/etc/test/Test1.java
+++ src/main/java/kr/co/choi/etc/test/Test1.java
@@ -1,14 +1,20 @@
 package kr.co.choi.etc.test;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.UUID;
+
+@Slf4j
 public class Test1 {
 
     public static void main(String[] args) {
-        System.out.println("test");
-        System.out.println("test");
-        System.out.println("test");
-
-        System.out.print("1");
-        System.out.print("1");
-        System.out.print("1");
+        UUID uuid = UUID.randomUUID();
+        UUID uuid2 = UUID.randomUUID();
+        UUID uuid3 = UUID.randomUUID();
+        UUID uuid4 = UUID.randomUUID();
+        log.info("### uuid = {}", uuid);
+        log.info("### uuid2 = {}", uuid2);
+        log.info("### uuid3 = {}", uuid3);
+        log.info("### uuid4 = {}", uuid4);
     }
 }
